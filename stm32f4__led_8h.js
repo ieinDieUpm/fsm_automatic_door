94b4fb1807c35d6568e44aaeb309308330d5f2ba
josueportiz <josueportiz@gmail.com>
2025-05-06 11:29:17 +0200

    Update LED connections and LED redifine

--- stm32f4__led_8h.js
+++ stm32f4__led_8h.js
@@ -1,13 +1,13 @@
 var stm32f4__led_8h =
 [
+    [ "STM32F4_LED_CLOSING_GPIO_PIN", "stm32f4__led_8h.html#a12815063e415e86d7c7627274daf5083", null ],
     [ "STM32F4_LED_CLOSING_GPIO_PORT", "stm32f4__led_8h.html#af11e7bd2f2fc6decf007b0f3d51249fa", null ],
-    [ "STM32F4_LED_CLOSING_PIN_PORT", "stm32f4__led_8h.html#ae82d397e47c4e3f3659f449a4a5c295e", null ],
     [ "STM32F4_LED_CLOSING_TIMER", "stm32f4__led_8h.html#ad53e6d8c3959e0f736acfcf89d873154", null ],
     [ "STM32F4_LED_CLOSING_TIMER_IRQ", "stm32f4__led_8h.html#aaac271ddaf3ba7829136a7862b361857", null ],
     [ "STM32F4_LED_CLOSING_TIMER_IRQ_PRIO", "stm32f4__led_8h.html#a19f3e7bd19938eb94aad24429c9c3b9c", null ],
     [ "STM32F4_LED_CLOSING_TIMER_IRQ_SUBPRIO", "stm32f4__led_8h.html#ab10b1e94a4d80428784807f625df6881", null ],
+    [ "STM32F4_LED_OPENING_GPIO_PIN", "stm32f4__led_8h.html#aa7cb642508b0dbc9bcde321b6e14abbf", null ],
     [ "STM32F4_LED_OPENING_GPIO_PORT", "stm32f4__led_8h.html#a46de854d5889c4a6ca8611b22d4a15a2", null ],
-    [ "STM32F4_LED_OPENING_PIN_PORT", "stm32f4__led_8h.html#a2fba2b0625d6978b43d67fb4a8aed21b", null ],
     [ "STM32F4_LED_OPENING_TIMER", "stm32f4__led_8h.html#a97bd4be3d1289c1fa44a6438d1d2ed4f", null ],
     [ "STM32F4_LED_OPENING_TIMER_IRQ", "stm32f4__led_8h.html#a0a3278e319e49acd95af5e03cc6e1b73", null ],
     [ "STM32F4_LED_OPENING_TIMER_IRQ_PRIO", "stm32f4__led_8h.html#aba36e43b7b1d337812e2ebf637f1396a", null ],
